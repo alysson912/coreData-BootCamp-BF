92222752a342809186a89ce51ed0dbd75cfd9e1b
Alysson Menezes <almenezes912@gmail.com>
2022-12-26 19:12:23 -0300

    projeto inicial

---
+++ FirebaseCoreData/FirebaseCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseCoreData
+//
+//  Created by ALYSSON DODO on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
